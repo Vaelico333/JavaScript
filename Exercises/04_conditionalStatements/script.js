fbed646b252b76302d2a175258f2f86e734a9e3c
Vaelico333 <darzoga@gmail.com>
2022-06-27 11:14:36 +0100

    Correction

--- Exercises/04_conditionalStatements/script.js
+++ Exercises/04_conditionalStatements/script.js
@@ -1,14 +1,14 @@
-let direction = 'left'
+let direction = 'left';
 
 if (direction=='left') {
-  console.log('Red planet')
+  console.log('Red planet');
 }
 else if (direction=='right') {
-  console.log('Purple planet')
+  console.log('Purple planet');
 }
 else if (direction=='backwards') {
-  console.log('Back home')
+  console.log('Back home');
 }
 else {
-  console.log('Crash!')
+  console.log('Crash!');
 }
