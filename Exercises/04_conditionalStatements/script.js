0a52ed0d8551fedf51af0700cc6a994ad5c62e7f
Vaelico333 <darzoga@gmail.com>
2022-06-27 11:01:16 +0100

    Easy example about the use of IF, ELSE IF and ELSE  statements

---
+++ Exercises/04_conditionalStatements/script.js
@@ -0,0 +1,14 @@
+let direction = 'left'
+
+if (direction=='left') {
+  console.log('Red planet')
+}
+else if (direction=='right') {
+  console.log('Purple planet')
+}
+else if (direction=='backwards') {
+  console.log('Back home')
+}
+else {
+  console.log('Crash!')
+}
