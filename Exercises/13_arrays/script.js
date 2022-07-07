c523a8465cea35656fa47271c06bbe071ffd0793
Vaelico333 <darzoga@gmail.com>
2022-07-07 01:43:56 +0100

    Arrays and some basic ways to modify them

---
+++ Exercises/13_arrays/script.js
@@ -0,0 +1,9 @@
+let animals = ['turtle', 'cat', 'dog', 'bird'];
+console.log('The array is:', animals);
+console.log('The third animal in the array is:', animals[2]);
+animals[1] = 'giraffe';
+console.log('The updated array is:', animals);
+animals.push('lizard');
+console.log('The updated array with a new animal is:', animals);
+animals.pop()
+console.log('The updated array with the last animal removed is:', animals);
