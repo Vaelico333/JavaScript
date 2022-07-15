8007a6c39acf9b0124c0c3b977a92add0588321b
Vaelico333 <darzoga@gmail.com>
2022-07-15 18:27:59 +0100

    Brief introduction into the DOM API

---
+++ Exercises/16_DOM_API/script.js
@@ -0,0 +1,11 @@
+// select
+let sign = document.getElementById('sign');
+console.log(sign.textContent);
+
+// modify
+sign.textContent = 'Welcome, travelers!';
+console.log(sign.textContent);
+// sign.innerHTML = sign.innerHTML + '<p>hello</p>';
+
+// change style
+sign.style.color = 'blue';
